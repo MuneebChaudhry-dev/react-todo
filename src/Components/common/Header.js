c39c2f285d79d51be6fcde901fd6e4ef61b3e534
MuneebChaudhry-dev <muneebchaudhry.dev@gmail.com>
2023-01-03 13:29:58 +0500

    Restructre  App

--- src/Components/common/Header.js
+++ src/Components/common/Header.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import Button from "./Button";
+
 // const Header = (props) => {
 //   return (
 //     <div>
@@ -12,11 +12,11 @@ const Header = ({ title, onAdd, showAdd }) => {
   return (
     <div className="header">
       <h1>{title}</h1>
-      <Button
+      {/* <Button
         onClick={onAdd}
         color={showAdd ? "red" : "green"}
         text={showAdd ? "Close" : "Add"}
-      />
+      /> */}
     </div>
   );
 };
